9063a1b65fad2a1ee98e23e332ba310d16a64f48
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-19 12:00:00 +0000

    stoker notFound & onError

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import { OpenAPIHono } from '@hono/zod-openapi'
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { notFound, onError } from 'stoker/middlewares';
 
+const app = new OpenAPIHono();
 
-const app = new OpenAPIHono()
+app.notFound(notFound);
+app.onError(onError);
+
+app.get('/err', (c) => {
+  throw new Error("What's this");
+});
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.json({ message: 'Hello from our API' });
+});
 
-export default app
+export default app;
